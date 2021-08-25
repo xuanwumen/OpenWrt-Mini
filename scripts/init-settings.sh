4674a9d6350853b214fef491d326810c4d24f11b
xuanwumen <45932714+xuanwumen@users.noreply.github.com>
2021-08-26 02:27:08 +0800

    Update init-settings.sh

--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -7,10 +7,14 @@
 #=================================================
 
 # Set default theme to luci-theme-argon
-# uci set luci.main.mediaurlbase='/luci-static/argon'
+uci set luci.main.mediaurlbase='/luci-static/argon'
 
 # Disable opkg signature check
 sed -i 's/option check_signature/# option check_signature/g' /etc/opkg.conf
+
+# Disable autostart by default for some packages
+cd /etc/rc.d
+rm -f S98udptools || true
 
 # Add build date to index page
 sed -i '/DISTRIB_REVISION/d' /etc/openwrt_release 
@@ -19,7 +23,6 @@ echo "DISTRIB_DESCRIPTION='OpenWrt '" >> /etc/openwrt_release
 export orig_version="$(cat "/etc/openwrt_release" | grep DISTRIB_DESCRIPTION | awk -F "'" '{print $2}')"
 sed -i "s/${orig_version}/${orig_version} By Vip $(date +"%Y-%m-%d")/g" /etc/openwrt_release
 
-# Add execute permission for ipv6-helper
-chmod +x /bin/ipv6-helper
+
 
 exit 0

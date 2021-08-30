6ed42fa584a1a73698fc8c9abc6acb37e5145083
xuanwumen <45932714+xuanwumen@users.noreply.github.com>
2021-08-30 13:36:31 +0800

    Update project-lede.sh

--- scripts/project-lede.sh
+++ scripts/project-lede.sh
@@ -35,6 +35,10 @@ git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
 git clone --depth=1 https://github.com/jerrykuku/lua-maxminddb.git
 git clone --depth=1 https://github.com/jerrykuku/luci-app-vssr
 
+# Add luci-app-bypass
+git clone --depth=1 https://github.com/garypang13/luci-app-bypass
+git clone --depth=1 https://github.com/garypang13/smartdns-le
+
 # Add mentohust & luci-app-mentohust
 git clone --depth=1 https://github.com/BoringCat/luci-app-mentohust
 git clone --depth=1 https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk
